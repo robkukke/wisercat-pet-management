bf4f68e1aa09b155a04bd9e6823c8f40b76c5ca2
Robin Kukke <robkukke@users.noreply.github.com>
2023-05-01 23:35:20 +0300

    Add 3 user accounts into database

---
+++ src/main/resources/db/changelog/changes/009-user-values.sql
@@ -0,0 +1,3 @@
+INSERT INTO account (username, password) VALUES ('userone', 'userone');
+INSERT INTO account (username, password) VALUES ('usertwo', 'usertwo');
+INSERT INTO account (username, password) VALUES ('userthree', 'userthree');
