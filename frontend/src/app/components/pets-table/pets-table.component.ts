ce5265724658dc6dff9672b21c6f1e8d67f0a443
robkukke <rkukke@tlu.ee>
2023-05-02 18:48:10 +0300

    Implement table sorting

--- frontend/src/app/components/pets-table/pets-table.component.ts
+++ frontend/src/app/components/pets-table/pets-table.component.ts
@@ -9,13 +9,43 @@ import { PetService } from '../../services/pet.service';
 })
 export class PetsTableComponent implements OnInit {
   pets!: Pet[];
-  headers = ['#', 'Name', 'Code', 'Type', 'Fur color', 'Country'];
+  petsToShow!: Pet[];
+  headers = ['ID', 'Name', 'Code', 'Type', 'Fur color', 'Country'];
+  sortOrder = { column: '', ascending: true };
 
   constructor(private petService: PetService) {}
 
   ngOnInit() {
     this.petService.getPets().subscribe((data) => {
       this.pets = data;
+      this.petsToShow = [...this.pets];
+    });
+  }
+
+  sort(column: string) {
+    const key = column
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase()) as keyof Pet;
+
+    if (this.sortOrder.column === column) {
+      this.sortOrder.ascending = !this.sortOrder.ascending;
+    } else {
+      this.sortOrder.column = column;
+      this.sortOrder.ascending = true;
+    }
+
+    this.petsToShow.sort((a, b) => {
+      const aValue = a[key];
+      const bValue = b[key];
+      let comparison = 0;
+
+      if (aValue > bValue) {
+        comparison = 1;
+      } else if (aValue < bValue) {
+        comparison = -1;
+      }
+
+      return this.sortOrder.ascending ? comparison : -comparison;
     });
   }
 }
